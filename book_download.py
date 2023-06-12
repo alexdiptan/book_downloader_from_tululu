4f323621e4ba188b1c48418ec3e917b9b87cef8d
alexdiptan <diptan@google.com>
2023-06-12 10:51:55 +0300

    add check_for_redirect func

--- book_download.py
+++ book_download.py
@@ -8,14 +8,26 @@ def save_book_to_file(url_content, file_name: str, path_to_save: Path):
         file.write(url_content.content)
 
 
+def check_for_redirect(url_response):
+    if url_response.history:
+        raise requests.HTTPError("Skip redirect page")
+
+
 def main():
-    url = "https://tululu.org/txt.php?id="
+    url = "https://tululu.org/txt.php"
     book_path = Path.cwd() / "books"
 
-    for book_id in range(1, 10):
+    for book_id in range(1, 11):
+        payload = {"id": book_id}
         filename = f"id_{book_id}.txt"
-        response = requests.get(url+str(book_id))
+        response = requests.get(url, params=payload)
         response.raise_for_status()
+
+        try:
+            check_for_redirect(response)
+        except requests.HTTPError:
+            print("This is not a book. Skip.")
+            continue
 
         if not book_path.exists():
             Path.mkdir(book_path)
