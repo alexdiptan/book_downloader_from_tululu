58d5a9e345eca51918831675ba52c3355b39cf9d
aleksandrdiptan <diptan@huntflow.ru>
2023-06-13 18:02:40 +0300

    download book covers

--- book_download.py
+++ book_download.py
@@ -1,3 +1,5 @@
+from urllib.parse import urljoin, urlsplit, urlparse, unquote
+
 from bs4 import BeautifulSoup
 from pathlib import Path
 from loguru import logger
@@ -6,22 +8,39 @@ import requests
 from pathvalidate import sanitize_filename
 
 
-def save_book_to_file(url_content, path_to_file: Path):
+def save_to_file(url_content, path_to_file: Path):
     with open(path_to_file, "wb") as file:
         file.write(url_content.content)
 
 
-def download_txt(url, filename, folder="books/"):
-    folder_path = Path.cwd() / folder
+def get_folder_path(folder_name: str) -> Path:
+    folder_path = Path.cwd() / folder_name
 
     if not Path(folder_path).exists():
         Path.mkdir(folder_path)
+
+    return folder_path
+
+
+def download_txt(url, filename, folder="books/"):
+    folder_path = get_folder_path(folder)
 
     file_path = Path(folder_path, sanitize_filename(filename))
 
     url_response = get_url_response(url)
 
-    save_book_to_file(url_response, file_path)
+    save_to_file(url_response, file_path)
+
+    return file_path
+
+
+def download_image(book_img_url: str, filename: str, folder="images/"):
+    folder_path = get_folder_path(folder)
+    file_path = Path(folder_path, sanitize_filename(filename))
+
+    url_response = get_url_response(book_img_url)
+
+    save_to_file(url_response, file_path)
 
     return file_path
 
@@ -40,7 +59,7 @@ def main():
     url = "https://tululu.org/"
 
     for book_id in range(1, 11):
-        book_page_url = f"{url}b{book_id}"
+        book_page_url = urljoin(url, f"b{book_id}")
         logger.info(f"Try to download book from page - {book_page_url}")
 
         try:
@@ -53,19 +72,23 @@ def main():
 
         book_title, author = soup.find("body").find("h1").text.split("::")
         filename = "{}. {}.txt".format(book_id, sanitize_filename(book_title).strip())
-        book_urls = soup.find(class_="d_book").find_all("a")
+        book_info = soup.find(class_="d_book")
+        book_urls = book_info.find_all("a")
+        book_img_url = urljoin(url, book_info.find('img')['src'])
+        book_img_name = urlsplit(book_img_url).path.split('/')[-1]
 
         txt_book_url = ""
         for book_url in book_urls:
             if book_url.text == "скачать txt":
-                txt_book_url = f"{url}{book_url['href']}"
+                txt_book_url = f'{url}{book_url["href"]}'
                 continue
 
         if len(txt_book_url) < 1:
             logger.warning("Book page does not contain txt link. Skip book.")
             continue
 
-        download_txt(txt_book_url, filename)
+        # download_txt(txt_book_url, filename)
+        download_image(book_img_url, book_img_name)
 
         logger.info(f"File {filename} saved successfully.")
 
